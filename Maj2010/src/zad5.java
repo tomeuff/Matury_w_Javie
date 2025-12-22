e01495e937c0b62ce0280a9097a175aee78ecd81
informatyk <informatyk@gmail.com>
2025-12-22 11:28:03 +0100

    Dokończenie matury maj 2010

--- Maj2010/src/zad5.java
+++ Maj2010/src/zad5.java
@@ -4,10 +4,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class zad5 {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(new File("Maj2010/Dane/pesel.txt"));
+        BufferedWriter bw_5 = new BufferedWriter(new FileWriter("C:\\Users\\Admin\\IdeaProjects\\Matury_w_Javie\\Maj2010\\wyniki\\odp_5.txt"));
         ArrayList<String> dane = new ArrayList<>();
         while(sc.hasNextLine())
         {
@@ -23,7 +25,8 @@ public class zad5 {
                 ile_grudzien++;
             }
         }
-        System.out.println(ile_grudzien);
+        bw_5.write("a)\n");
+        bw_5.write(String.format("%d\n\n", ile_grudzien));
 
         //Zadanie b)
         int ile_kobiet = 0;
@@ -34,31 +37,74 @@ public class zad5 {
                 ile_kobiet++;
             }
         }
-        System.out.println(ile_kobiet);
+        bw_5.write("b)\n");
+        bw_5.write(String.format("%d\n\n", ile_kobiet));
 
         //Zadanie c)
-        HashMap<String, Integer> hm = new HashMap<>();
+        HashMap<String, Integer> rok_urodzenia = new HashMap<>();
         for(String p : dane)
         {
             String rok = p.substring(0, 2);
-            if(hm.containsKey(rok))
+            if(rok_urodzenia.containsKey(rok))
             {
-                hm.put(rok, hm.get(rok) + 1);
+                rok_urodzenia.put(rok, rok_urodzenia.get(rok) + 1);
             }
             else
             {
-                hm.put(rok, 1);
+                rok_urodzenia.put(rok, 1);
             }
         }
-        String max_rok = hm.entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse(null);
-        System.out.println("19" + max_rok);
+        String max_rok = rok_urodzenia.entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse(null);
+        bw_5.write("c)\n");
+        bw_5.write(String.format("%s\n\n", "19" + max_rok));
 
         //Zadanie d)
         int[] wagi = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+        ArrayList<String> bledne_pesele = new ArrayList<>();
         for(String p : dane)
         {
-            pesel = p.chars().map(c -> c - '0').toArray();
-            System.out.println(pesel);
+            int[] pesel = p.chars().map(c -> c - '0').toArray();
+            int cyfra_kontrolna = IntStream.range(0, wagi.length).map(i-> wagi[i] * pesel[i]).sum() % 10;
+            cyfra_kontrolna = (cyfra_kontrolna == 0) ? 0 : 10 - cyfra_kontrolna;
+            if(cyfra_kontrolna != pesel[pesel.length - 1])
+            {
+                bledne_pesele.add(Arrays.stream(pesel).mapToObj(String::valueOf).collect(Collectors.joining()));
+            }
         }
+        Collections.sort(bledne_pesele);
+
+        bw_5.write("d)\n");
+
+        for(String bp : bledne_pesele)
+        {
+            bw_5.write(String.format("%s\n", bp));
+        }
+        bw_5.write("\n");
+
+        //Zadanie e)
+        Map<Integer, Integer> dziesieciolecie_urodzenia = new TreeMap<>();
+        for(String p : dane)
+        {
+            int dziesieciolecie = ((int)p.charAt(0) - '0') * 10;
+            if(dziesieciolecie_urodzenia.containsKey(dziesieciolecie))
+            {
+                dziesieciolecie_urodzenia.put(dziesieciolecie, dziesieciolecie_urodzenia.get(dziesieciolecie) + 1);
+            }
+            else
+            {
+                dziesieciolecie_urodzenia.put(dziesieciolecie, 1);
+            }
+        }
+
+        bw_5.write("e)\n");
+
+        for(Map.Entry<Integer, Integer> kv : dziesieciolecie_urodzenia.entrySet())
+        {
+            int dziesieciolecie = kv.getKey();
+            int urodzeni = kv.getValue();
+            bw_5.write(String.format("%d %d\n", dziesieciolecie, urodzeni));
+        }
+
+        bw_5.close();
     }
 }
---
+++ Maj2010/src/zad6.java
@@ -0,0 +1,240 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+class Uczen
+{
+    int idUcznia;
+    String nazwisko;
+    String imie;
+    String ulica;
+    String dom;
+    String idKlasy;
+
+
+    public Uczen(int idUcznia, String nazwisko, String imie, String ulica, String dom, String idKlasy) {
+        this.idUcznia = idUcznia;
+        this.nazwisko = nazwisko;
+        this.imie = imie;
+        this.ulica = ulica;
+        this.dom = dom;
+        this.idKlasy = idKlasy;
+    }
+}
+
+class Ocena
+{
+    int idUcznia;
+    int ocena;
+    String data;
+    int idPrzedmiotu;
+
+    public Ocena(int idUcznia, int ocena, String data, int idPrzedmiotu) {
+        this.idUcznia = idUcznia;
+        this.ocena = ocena;
+        this.data = data;
+        this.idPrzedmiotu = idPrzedmiotu;
+    }
+}
+
+class Przedmiot
+{
+    int idPrzedmiotu;
+    String nazwaPrzedmiotu;
+    String nazwisko_naucz;
+    String imie_naucz;
+
+    public Przedmiot(int idPrzedmiotu, String nazwaPrzedmiotu, String nazwisko_naucz, String imie_naucz) {
+        this.idPrzedmiotu = idPrzedmiotu;
+        this.nazwaPrzedmiotu = nazwaPrzedmiotu;
+        this.nazwisko_naucz = nazwisko_naucz;
+        this.imie_naucz = imie_naucz;
+    }
+}
+
+class Para
+{
+    int lKobiet;
+    int lMezczyzn;
+
+    public Para(int lKobiet, int lMezczyzn) {
+        this.lKobiet = lKobiet;
+        this.lMezczyzn = lMezczyzn;
+    }
+}
+
+class PrzedmiotSrednia
+{
+    String przedmiot;
+    float srednia;
+
+    public PrzedmiotSrednia(String przedmiot, float srednia) {
+        this.przedmiot = przedmiot;
+        this.srednia = srednia;
+    }
+}
+
+public class zad6 {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Uczen> uczniowie = new ArrayList<>();
+        ArrayList<Ocena> oceny = new ArrayList<>();
+        ArrayList<Przedmiot> przedmioty = new ArrayList<>();
+
+        Scanner sc_uczniowie = new Scanner(new File("C:\\Users\\Admin\\IdeaProjects\\Matury_w_Javie\\Maj2010\\Dane\\uczniowie.txt"));
+        Scanner sc_oceny = new Scanner(new File("C:\\Users\\Admin\\IdeaProjects\\Matury_w_Javie\\Maj2010\\Dane\\oceny.txt"));
+        Scanner sc_przedmioty = new Scanner(new File("C:\\Users\\Admin\\IdeaProjects\\Matury_w_Javie\\Maj2010\\Dane\\przedmioty.txt"));
+
+        sc_uczniowie.nextLine();
+        while(sc_uczniowie.hasNext())
+        {
+            String[] linia = sc_uczniowie.nextLine().split(";");
+            uczniowie.add(new Uczen(Integer.parseInt(linia[0]), linia[1], linia[2], linia[3], linia[4], linia[5]));
+        }
+
+        sc_oceny.nextLine();
+        while(sc_oceny.hasNext())
+        {
+            String[] linia = sc_oceny.nextLine().split(";");
+            oceny.add(new Ocena(Integer.parseInt(linia[0]), Integer.parseInt(linia[1]), linia[2], Integer.parseInt(linia[3])));
+        }
+
+        sc_przedmioty.nextLine();
+        while(sc_przedmioty.hasNext())
+        {
+            String[] linia = sc_przedmioty.nextLine().split(";");
+            przedmioty.add(new Przedmiot(Integer.parseInt(linia[0]), linia[1], linia[2], linia[3]));
+        }
+
+        //Zadanie a)
+        int ile_poza_rejonem = 0;
+
+        for(Uczen u : uczniowie)
+        {
+            if(u.ulica.equals("Worcella") || u.ulica.equals("Sportowa"))
+            {
+                ile_poza_rejonem++;
+            }
+        }
+
+        System.out.println(ile_poza_rejonem);
+
+        //Zadanie b)
+        int id_jana_augustyniaka = uczniowie.stream().filter(c -> c.imie.equals("Jan") && c.nazwisko.equals("Augustyniak")).map(c -> c.idUcznia).findFirst().orElse(null);
+        int id_jezyka_polskiego = przedmioty.stream().filter(c -> c.nazwaPrzedmiotu.equals("polski")).map(c -> c.idPrzedmiotu).findFirst().orElse(null);
+        int[] oceny_ja_jp = oceny.stream().filter(c -> c.idUcznia == id_jana_augustyniaka && c.idPrzedmiotu == id_jezyka_polskiego).mapToInt(c -> c.ocena).toArray();
+
+        for(int o : oceny_ja_jp)
+        {
+            System.out.println(o);
+        }
+
+        //Zadanie c)
+        TreeMap<String, Para> kkm = new TreeMap<>();
+
+        for(Uczen u : uczniowie)
+        {
+            if(kkm.containsKey(u.idKlasy))
+            {
+                if(u.imie.endsWith("a"))
+                {
+                    kkm.put(u.idKlasy, new Para(kkm.get(u.idKlasy).lKobiet + 1, kkm.get(u.idKlasy).lMezczyzn));
+                }
+                else
+                {
+                    kkm.put(u.idKlasy, new Para(kkm.get(u.idKlasy).lKobiet, kkm.get(u.idKlasy).lMezczyzn + 1));
+                }
+            }
+            else
+            {
+                if(u.imie.endsWith("a"))
+                {
+                    kkm.put(u.idKlasy, new Para(1, 0));
+                }
+                else
+                {
+                    kkm.put(u.idKlasy, new Para(0, 1));
+                }
+            }
+        }
+
+        for(Map.Entry<String, Para> p : kkm.entrySet())
+        {
+            System.out.printf("%s %d %d\n", p.getKey(), p.getValue().lKobiet, p.getValue().lMezczyzn);
+        }
+
+        //Zadanie d)
+        ArrayList<PrzedmiotSrednia> ps = new ArrayList<>();
+        for(Przedmiot p : przedmioty)
+        {
+            String przedmiot = p.nazwaPrzedmiotu;
+            int liczba_ocen = 0;
+            float suma_ocen = 0.0f;
+            for(Uczen u : uczniowie)
+            {
+                if(u.idKlasy.equals("2a"))
+                {
+                    for(Ocena o : oceny)
+                    {
+                        if(o.idUcznia == u.idUcznia && o.idPrzedmiotu == p.idPrzedmiotu)
+                        {
+                            suma_ocen += o.ocena;
+                            liczba_ocen++;
+                        }
+                    }
+                }
+            }
+            ps.add(new PrzedmiotSrednia(przedmiot, Float.parseFloat(String.format("%.2f", suma_ocen / liczba_ocen).replace(',', '.'))));
+        }
+
+        ps.sort(Comparator.comparingDouble(c -> -c.srednia));
+
+        for(PrzedmiotSrednia p : ps)
+        {
+            System.out.printf("%s %f\n", p.przedmiot, p.srednia);
+        }
+
+        //Zadanie e)
+        for(Uczen u : uczniowie)
+        {
+            if(u.idKlasy.equals("2c"))
+            {
+                for(Przedmiot p : przedmioty)
+                {
+                    for(Ocena o : oceny)
+                    {
+                        if(u.idUcznia == o.idUcznia && p.idPrzedmiotu == o.idPrzedmiotu)
+                        {
+                            if(o.data.startsWith("2009-04") && o.ocena == 1)
+                            {
+                                System.out.printf("%s %s %s\n", u.imie, u.nazwisko, p.nazwaPrzedmiotu);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //Zadanie f)
+        Float[] srednie = new Float[uczniowie.size()];
+
+        for(Uczen u : uczniowie)
+        {
+            int liczba_ocen = 0;
+            srednie[u.idUcznia - 1] = 0.0f;
+            for(Ocena o : oceny)
+            {
+                if(u.idUcznia == o.idUcznia)
+                {
+                    liczba_ocen++;
+                    srednie[u.idUcznia - 1] += (float)o.ocena;
+                }
+            }
+            srednie[u.idUcznia - 1] = (float)srednie[u.idUcznia - 1] / (float)liczba_ocen;
+        }
+
+        float max_srednia = Collections.max(Arrays.asList(srednie));
+        int ktory_uczen = Arrays.asList(srednie).indexOf(max_srednia);
+        System.out.printf("%s %s %s %f\n", uczniowie.get(ktory_uczen).imie, uczniowie.get(ktory_uczen).nazwisko, uczniowie.get(ktory_uczen).idKlasy, max_srednia);
+    }
+}
